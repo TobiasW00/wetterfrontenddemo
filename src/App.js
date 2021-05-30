b99ad0e8c6c0cfc397dbb8c51f7b1115eaffa681
TobiasW00 <Tobias.Wolter@standard-cloud.com>
2021-05-30 14:33:29 +0200

    logintest

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { AuthenticatedTemplate, UnauthenticatedTemplate ,useIsAuthenticated,useMsal,MsalAuthenticationTemplate} from "@azure/msal-react";
+import { InteractionType } from "@azure/msal-browser";
+function ErrorComponent({error}) {
+  return <p>An Error Occurred: {error}</p>;
+}
+
+function LoadingComponent() {
+  return <p>Authentication in progress...</p>;
+}
+
+
+// https://graph.microsoft.com/v1.0/me
+
 
 function App() {
+  const { instance, accounts, inProgress } = useMsal();
+  const isAuthenticated = useIsAuthenticated();
+  console.log(isAuthenticated);
+
+
+    const authRequest = {
+        scopes: ["openid", "profile"]
+    };
+
+console.log(accounts);
+console.log(instance);
+console.log(inProgress);
+
   return (
-    <div className="App">
-      <div>
-      <header className="App-header">
-      </header>
-    </div>
+    <div className="ss">
+
+    {isAuthenticated}
     <div>
-      Test
+    <MsalAuthenticationTemplate 
+            interactionType={InteractionType.Popup} 
+            authenticationRequest={authRequest} 
+            errorComponent={ErrorComponent} 
+            loadingComponent={LoadingComponent}
+        >
+            <p>At least one account is signed in!</p>
+        </MsalAuthenticationTemplate>
+
+    <AuthenticatedTemplate>
+                <p>At least one account is signed in!</p>
+            </AuthenticatedTemplate>
+            <UnauthenticatedTemplate>
+                <p>No users are signed in!</p>
+            </UnauthenticatedTemplate>
     </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MsalProvider } from "@azure/msal-react";
+import { PublicClientApplication } from "@azure/msal-browser";
+
+
+// MSAL configuration
+const configuration = {
+    auth: {
+        clientId: "0ca30df2-8879-425a-aa5f-9b9198791e26"
+    }
+};
+
+const pca = new PublicClientApplication(configuration);
 
 ReactDOM.render(
   <React.StrictMode>
+    <MsalProvider instance={pca}>
     <App />
+    </MsalProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
